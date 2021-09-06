180c44cc5ecf2a6c163df7e37372e5218d9096e2
TG2408 <tanmaygupta2408@gmail.com>
2021-09-06 21:50:14 +0530

    some modification

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,13 @@ addEventListener("keydown", function(e) {
     let sound = this.document.querySelector(`audio[data-key="${e.keyCode}"]`) ;
     if(sound != undefined && sound != null) {
         this.document.querySelector(`div[data-key="${e.keyCode}"]`).setAttribute("class", "key playing");
+        sound.currentTime = 0; //revind to start
         sound.play();
+
+        /* 
+        during playtime of audio sound.play() will not entertain a new key, 
+        therefore we set sound.currentTime = 0 on every keydown
+         */
     }
     else return;
 })
